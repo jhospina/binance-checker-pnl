db19a30c0cdb72f4d0f933d933a066313ff9a9b3
John Ospina <jhonospina150@gmail.com>
2025-02-01 08:15:38 -0500

    show logs

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Show laravel logs
+Route::get("/log", function () {
+    return response()->file(storage_path("logs/laravel.log"));
+});
